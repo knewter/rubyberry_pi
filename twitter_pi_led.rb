3fc3586e2a08c6c6d14c91913c9a2dd73afeb1a3
Josh Adams <josh@isotope11.com>
2013-03-28 21:09:32 -0500

    lawlz bugfickses

--- twitter_pi_led.rb
+++ twitter_pi_led.rb
@@ -34,12 +34,14 @@ class ToggleHandler
   def toggle_on_search
     SearchesTweets.new('isotopeled').search do |tweets|
       tweet = tweets[0]
-      if(tweet["id_str"] != last_tweet_id)
-        STDOUT.puts count
-        STDOUT.puts tweet.inspect
-        last_tweet_id = tweet["idstr"]
-        @count += 1
-        count.even? ? pin.on : pin.off
+      if(tweet)
+        if(tweet["id_str"] != last_tweet_id)
+          STDOUT.puts count
+          STDOUT.puts tweet.inspect
+          last_tweet_id = tweet["id_str"]
+          @count += 1
+          count.even? ? pin.on : pin.off
+        end
       end
     end
   end
